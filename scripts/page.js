5f2bd26499739f4fa8cfac2de942537070b47035
Calvin Wood <cjrakau2018@gmail.com>
2021-01-12 08:51:25 -0700

    updated java and index

--- scripts/page.js
+++ scripts/page.js
@@ -1,7 +1,9 @@
-window.addEventListener('load', (event)=>{
+window.addEventListener("load", (event)=>{
     const lu = document.querySelector("#lastupdated");
-    lu.testContent = document.lastModified;
+    lu.textContent = document.lastModified;
 
     const cry = document.querySelector("#copyrightyear");
-    cry.textContent = new Data().getFullYear();
+    cry.textContent = new Date().getFullYear();
+    
 })
+
